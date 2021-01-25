e6deaaf255cb0fcdf73a73c7a80e41425affb509
crypto <train.hartnet7@gmail.com>
2021-01-25 05:07:39 +0200

    creating the gome page

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express);
 
 app.get('/', (req, res, next) => {
     res.send('Hello world');
 });
-
+app.get('/home', (req, res, next) =>{
+    res.render('home', null);
+});
 app.listen(3000); //3000, 5000, 8000, 8080
